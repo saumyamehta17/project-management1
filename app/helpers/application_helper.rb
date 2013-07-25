d753118ca8d0f024411f92d393e64d01b4f4b57d
sweety <smehta1289@gmail.com>
2013-07-25 23:43:54 +0530

    added gravatar feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def avatar_url(user)
+		default_url = "#{root_url}images/fb.jpg" 
+		gravatar_id = Digest::MD5.hexdigest(user.email).downcase
+		return "http://gravatar.com/avatar/#{gravatar_id}.png?s=30"
+	end
+
 end
+
+
